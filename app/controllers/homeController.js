1177d497ff82b7fee0dfaf0cb613984d13dd6e5b
Ynck-Hng <ynckhuang@gmail.com>
2023-01-23 23:38:48 +0100

    home page

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,11 +1,37 @@
+const { Sequelize } = require("sequelize");
+const sequelize = require("../models/database/sequelize");
 const {errorCatcher} = require("./../middlewares/errorHandler/errorHandler");
-const {Figurine} = require("./../models");
+const {Figurine, Review} = require("./../models");
 
 const homeController = {
 
-    homePage: async (req,res) => {
-        res.render("home");
-    },
+    homePage: errorCatcher(async (req,res) => {
+
+        const result = await Review.findAll({
+            attributes: [[sequelize.fn('AVG', sequelize.col("note")), "note"]],
+            group: ["figurine.id"],
+            order: [['note', 'DESC']],
+            include:"figurine",
+        });
+        const categories = [];
+        const categoriesCountTotal = [];
+        for(let element of result){
+            categories.push(element.figurine.category);
+        }
+        const uniqueCategories = new Set(categories);
+        for(let category of uniqueCategories){
+            let countedCategories = [];
+            if(!countedCategories.find(element => element === category)){
+                let {count} = await Figurine.findAndCountAll({
+                    where:{
+                        category: `${category}`
+                    }
+                })
+                categoriesCountTotal.push({name: `${category}`, count});
+            }
+        }
+        res.render("home", {figurines: result, categories: categoriesCountTotal});
+    }),
 
     
 
