6d23be2b59852589df617530cefdde50331a7985
Ynck-Hng <ynckhuang@gmail.com>
2023-01-24 00:09:16 +0100

    DYNAMISATION - item page

--- app/controllers/itemController.js
+++ app/controllers/itemController.js
@@ -1,19 +1,26 @@
+const sequelize = require("./../models/database/sequelize");
 const {errorCatcher} = require("./../middlewares/errorHandler/errorHandler");
 const {Review} = require("./../models");
 const itemController = {
 
     itemPage: errorCatcher(async (req,res)=>{
         const figurineId = Number(req.params.figurineId);
-        const result = await Review.findAll({
+        const figurineInfo = await Review.findAll({
+            attributes: [[sequelize.fn("AVG", sequelize.col("note")), "note"]],
+            group: ["figurine.id"],
             where:{
                 figurine_id: figurineId
             },
             include: "figurine"
         })
 
-        console.log(JSON.stringify(result));
+        const reviews = await Review.findAll({
+            where:{
+                figurine_id: figurineId,
+            }
+        })
 
-        res.render("item");
+        res.render("item", {reviews, figurine: figurineInfo[0].figurine, rating: figurineInfo[0].note});
     }),
 
 }
