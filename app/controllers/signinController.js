4233711292a392acb82cd78bea8f9836955d804e
Ynck-Hng <ynckhuang@gmail.com>
2023-01-24 14:17:27 +0100

    DYNAMISATION - signin début

--- app/controllers/signinController.js
+++ app/controllers/signinController.js
@@ -1,8 +1,14 @@
+const {errorCatcher} = require("./../middlewares/errorHandler/errorHandler");
+const {User} = require("./../models");
 const signinController = {
     signinPage: (req,res)=>{
+        let errorSignin = [];
         res.render("signin");
-    }
+    },
 
+    signinSuccess: errorCatcher(async(req,res)=>{
+        
+    })
 }
 
 module.exports = signinController;
